2449f26c60c3da4a75286c7781dbfc835a65236a
dudangel <eduarda.crsa@gmail.com>
2021-06-20 16:22:27 -0300

    Iterative newton method exercises

--- ANN/newtonIter.c
+++ ANN/newtonIter.c
@@ -1,16 +1,145 @@
 #include <stdio.h>
+#include <math.h>
 
 //Método iterativo de newton para sistemas não lineares
 
 int main(){
-    
+    // Exemplo da aula
+    // //equações e derivadas parciais
+    // double f1(double x, double y){
+    //     return x * x  + y * y - 2.0;
+    // }
+
+    // double f2(double x, double y){
+    //     return x *x - y - 1.0;
+    // }
+
+    // double f1x(double x, double y){
+    //     return 2.0 * x;        
+    // }
+
+    // double f1y(double x, double y){
+    //     return 2.0 * y;
+    // }
+
+    // double f2x(double x, double y){
+    //     return 2.0 * x;
+    // }
+
+    // double f2y(double x, double y){
+    //     return -1.0;
+    // }
+
+    // //estimativas iniciais e nro de iterações
+    // double x0 = 1;
+    // double y0 = 0;
+
+    // int n = 8;
+
+    // Exercicio 47
+    // //equações e derivadas parciais
+    // double f1(double x, double y){
+    //     return x * x  + 2 * (y * y) - 3.0;
+    // }
+
+    // double f2(double x, double y){
+    //     return 4 * (x * x) + y * y - 6.0;
+    // }
+
+    // double f1x(double x, double y){
+    //     return 2.0 * x;        
+    // }
+
+    // double f1y(double x, double y){
+    //     return 4.0 * y;
+    // }
+
+    // double f2x(double x, double y){
+    //     return 8.0 * x;
+    // }
+
+    // double f2y(double x, double y){
+    //     return 2.0 * y;
+    // }
+
+    // //estimativas iniciais e nro de iterações
+    // double x0 = 1.28;
+    // double y0 = -0.5905;
+
+    // int n = 4;
+
+    // Exercicio 48
+    // //equações e derivadas parciais
+    // double f1(double x, double y){
+    //     return 4 * (x * x) + y * y - 5.0;
+    // }
+
+    // double f2(double x, double y){
+    //     return x * x + y * y * y - 4.0;
+    // }
+
+    // double f1x(double x, double y){
+    //     return 8.0 * x;        
+    // }
+
+    // double f1y(double x, double y){
+    //     return 2.0 * y;
+    // }
+
+    // double f2x(double x, double y){
+    //     return 2.0 * x;
+    // }
+
+    // double f2y(double x, double y){
+    //     return 3.0 * y * y;
+    // }
+
+    // //estimativas iniciais e nro de iterações
+    // double x0 = -0.7022;
+    // double y0 = 1.3134;
+
+    // int n = 4;
+
+    // Exercicio 49
+    // //equações e derivadas parciais
+    // double f1(double x, double y){
+    //     return x * x + y * y - 5.0;
+    // }
+
+    // double f2(double x, double y){
+    //     return x * x + x * pow(y,3) - 3.0;
+    // }
+
+    // double f1x(double x, double y){
+    //     return 2.0 * x;        
+    // }
+
+    // double f1y(double x, double y){
+    //     return 2.0 * y;
+    // }
+
+    // double f2x(double x, double y){
+    //     return 2.0 * x + pow(y,3);
+    // }
+
+    // double f2y(double x, double y){
+    //     return 3.0 * x * pow(y,2);
+    // }
+
+    // //estimativas iniciais e nro de iterações
+    // double x0 = -0.5286;
+    // double y0 = -1.8305;
+
+    // int n = 4;
+
+    // Exercicio 50
     //equações e derivadas parciais
     double f1(double x, double y){
-        return x * x  + y * y - 2.0;
+        return x * x + y * y - 1.0;
     }
 
     double f2(double x, double y){
-        return x *x - y - 1.0;
+        return x - y - 0.0;
     }
 
     double f1x(double x, double y){
@@ -22,7 +151,7 @@ int main(){
     }
 
     double f2x(double x, double y){
-        return 2.0 * x;
+        return 1.0;
     }
 
     double f2y(double x, double y){
@@ -30,10 +159,10 @@ int main(){
     }
 
     //estimativas iniciais e nro de iterações
-    double x0 = 1;
-    double y0 = 0;
+    double x0 = -0.635;
+    double y0 = -0.5769;
 
-    int n = 8;
+    int n = 4;
 
     //!!! Não alterar a partir daqui !!!
     double det(double x, double y){
